7334d4374e8591abb8bd6c94079120b3ac432361
Venkata <venkata@yombu.com>
2018-10-29 16:39:49 -0400

    Completed pulling metrics from RDS

--- Metrics/__init__.py
+++ Metrics/__init__.py
@@ -1,66 +1,147 @@
 # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/MonitoringOverview.html#monitoring-cloudwatch
 # https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/Aurora.Monitoring.html
+from datetime import datetime, timedelta
 
+import boto3
 from prometheus_client import Gauge
 
-LABELS = [
-    "db_name",
-    "db_engine",
-    "az_name"
-]
+from Logger.Logger import Logger
 
 
-Metrics = {
-    "maria": {
-        "write_latency"                 : Gauge("rds_exporter_write_latency_seconds", "The average amount of time taken per disk I/O operation", LABELS),
-        "read_latency"                  : Gauge("rds_exporter_read_latency_seconds", "The average amount of time taken per disk I/O operation", LABELS),
-        "write_iops"                    : Gauge("rds_exporter_write_IOPS_per_second", "The average number of disk write I/O operations per second", LABELS),
-        "read_iops"                     : Gauge("rds_exporter_read_IOPS_per_second", "The average number of disk read I/O operations per second", LABELS),
-        "read_throughput"               : Gauge("rds_exporter_read_throughput_bytes_per_second", "The average number of bytes read from disk per second", LABELS),
-        "write_throughput"              : Gauge("rds_exporter_read_throughput_bytes_per_second", "The average number of bytes written to disk per second", LABELS),
-        "cpu_credit_balance"            : Gauge("rds_exporter_cpu_credit_count", "CPU credit count", LABELS),
-        "burst_balance"                 : Gauge("rds_exporter_cpu_burst_balance_percent", "The percent of General Purpose SSD (gp2) burst-bucket I/O credits available", LABELS),
-        "network_transmit_throughput"   : Gauge("rds_exporter_network_transmit_throughput_bytes_per_second", "Network transmission throughput", LABELS),
-        "network_receive_throughput"    : Gauge("rds_exporter_network_receive_throughput_bytes_per_second", "Network receive throughput", LABELS),
-        "free_storage_space"            : Gauge("rds_exporter_free_space_bytes", "The amount of available storage space", LABELS),
-        "disk_queue_depth"              : Gauge("rds_exporter_disk_queue_depth_count", "The number of outstanding IOs (read/write requests) waiting to access the disk.", LABELS),
-        "cpu_utilization"               : Gauge("rds_exporter_cpu_utilization_percent", "The percentage of CPU utilization", LABELS),
-        "database_connections"          : Gauge("rds_exporter_database_connections_count", "The number of database connections in use", LABELS),
-        "swap_usage"                    : Gauge("rds_exporter_swap_usage_bytes", "The amount of swap space used on the DB instance", LABELS),
-        "freeable_memory"               : Gauge("rds_exporter_freeable_memory_bytes", "Available RAM", LABELS),
-        "binlog_disk_usage"             : Gauge("rds_exporter_binlog_disk_usage_bytes", "The amount of disk space occupied by binary logs on the master", LABELS)
-    },
+class Metrics:
+    NAMESPACE = "AWS/RDS"
 
-    "aurora": {
-        "queries"                       : Gauge("rds_exporter_queries_per_second", "The average number of queries executed per second", LABELS),
-        "deadlocks"                     : Gauge("rds_exporter_deadlock_per_second", "The average number of deadlocks in the database per second", LABELS),
-        "delete_latency"                : Gauge("rds_exporter_delete_latency_milliseconds", "The amount of latency for delete queries", LABELS),
-        "dml_throughput"                : Gauge("rds_exporter_dml_throughput_per_second", "The average number of inserts, updates, and deletes per second", LABELS),
-        "commit_latency"                : Gauge("rds_exporter_commit_latency_milliseconds", "The amount of latency for commit queries", LABELS),
-        "commit_throughput"             : Gauge("rds_exporter_commit_throughput_per_second", "The average number of commit queries per second", LABELS),
-        "blocked_transaction"           : Gauge("rds_exporter_blocked_transaction_per_second", "The average number of transactions in the database that are blocked per second", LABELS),
-        "select_throughput"             : Gauge("rds_exporter_select_throughput_per_second", "The average number of select queries per second", LABELS),
-        "select_latency"                : Gauge("rds_exporter_select_latency_milliseconds", "The amount of latency select requests", LABELS),
-        "ddl_latency"                   : Gauge("rds_exporter_ddl_latency_milliseconds", "The amount of latency for data definition language (DDL) requests", LABELS),
-        "network_transmit_throughput"   : Gauge("rds_exporter_network_transmit_throughput_bytes_per_second", "Network transmission throughput", LABELS),
-        "active_transactions"           : Gauge("rds_exporter_active_transactions_per_second", "The average number of current transactions executing on an Aurora database instance per second", LABELS),
-        "engine_uptime"                 : Gauge("rds_exporter_engine_uptime_seconds", "The amount of time that the instance has been running", LABELS),
-        "cpu_utilization"               : Gauge("rds_exporter_cpu_utilization_percent", "The percentage of CPU utilization", LABELS),
-        "dml_latency"                   : Gauge("rds_exporter_dml_latency_milliseconds", "The amount of latency for data manipulation language (DDL) requests", LABELS),
-        "delete_throughput"             : Gauge("rds_exporter_delete_throughput_per_second", "The average number of delete queries per second", LABELS),
-        "database_connections"          : Gauge("rds_exporter_database_connections_count", "The number of database connections in use", LABELS),
-        "freeable_memory"               : Gauge("rds_exporter_freeable_memory_bytes", "Available RAM", LABELS),
-        "ddl_throughput"                : Gauge("rds_exporter_ddl_throughput_per_second", "The average number of inserts, updates, and deletes per second", LABELS),
-        "insert_latency"                : Gauge("rds_exporter_insert_latency_milliseconds", "The amount of latency insert requests", LABELS),
-        "login_failures"                : Gauge("rds_exporter_login_failures_per_second", "The average number of failed login attempts per second", LABELS),
-        "free_local_storage"            : Gauge("rds_exporter_free_local_storage_bytes", "The amount of storage available for temporary tables and logs", LABELS),
-        "cpu_credit_balance"            : Gauge("rds_exporter_cpu_credit_count", "CPU credit count", LABELS),
-        "network_receive_throughput"    : Gauge("rds_exporter_network_receive_throughput_bytes_per_second", "Network receive throughput", LABELS),
-        "update_latency"                : Gauge("rds_exporter_update_latency_milliseconds", "The amount of latency update requests", LABELS),
-        "update_throughput"             : Gauge("rds_exporter_update_throughput_per_second", "The average number of update queries per second", LABELS),
-        "aurora_replica_lag_minimum"    : Gauge("rds_exporter_aurora_replica_lag_minimum_milliseconds", "The minimum amount of lag between the primary instance and each Aurora DB instance in the DB cluster", LABELS),
-        "aurora_replica_lag_maximum"    : Gauge("rds_exporter_aurora_replica_lag_maximum_milliseconds", "The maximum amount of lag between the primary instance and each Aurora DB instance in the DB cluster", LABELS),
-        "aurora_replica_lag"            : Gauge("rds_exporter_aurora_replica_lag_milliseconds", "The amount of lag when replicating updates from the primary instance", LABELS),
-        "aurora_bin_log_replica_lag"    : Gauge("rds_exporter_aurora_bin_log_replica_lag_seconds", "The amount of time a replica DB cluster running on Aurora with MySQL compatibility lags behind the source DB cluster", LABELS)
+    LABELS = [
+        "db_name",
+        "db_engine",
+        "az_name"
+    ]
+
+    METRICS = {
+        "maria": {
+            "WriteLatency"                 : Gauge("rds_exporter_maria_write_latency_seconds", "The average amount of time taken per disk I/O operation", LABELS),
+            "ReadLatency"                  : Gauge("rds_exporter_maria_read_latency_seconds", "The average amount of time taken per disk I/O operation", LABELS),
+            "WriteIOPS"                    : Gauge("rds_exporter_maria_write_IOPS_per_second", "The average number of disk write I/O operations per second", LABELS),
+            "ReadIOPS"                     : Gauge("rds_exporter_maria_read_IOPS_per_second", "The average number of disk read I/O operations per second", LABELS),
+            "ReadThroughput"               : Gauge("rds_exporter_maria_read_throughput_bytes_per_second", "The average number of bytes read from disk per second", LABELS),
+            "WriteThroughput"              : Gauge("rds_exporter_maria_write_throughput_bytes_per_second", "The average number of bytes written to disk per second", LABELS),
+            "CPUCreditBalance"             : Gauge("rds_exporter_maria_cpu_credit_count", "CPU credit count", LABELS),
+            "BurstBalance"                 : Gauge("rds_exporter_maria_burst_balance_percent", "The percent of General Purpose SSD (gp2) burst-bucket I/O credits available", LABELS),
+            "NetworkTransmitThroughput"    : Gauge("rds_exporter_maria_network_transmit_throughput_bytes_per_second", "Network transmission throughput", LABELS),
+            "NetworkReceiveThroughput"     : Gauge("rds_exporter_maria_network_receive_throughput_bytes_per_second", "Network receive throughput", LABELS),
+            "FreeStorageSpace"             : Gauge("rds_exporter_maria_free_space_bytes", "The amount of available storage space", LABELS),
+            "DiskQueueDepth"               : Gauge("rds_exporter_maria_disk_queue_depth_count", "The number of outstanding IOs (read/write requests) waiting to access the disk.", LABELS),
+            "CPUUtilization"               : Gauge("rds_exporter_maria_cpu_utilization_percent", "The percentage of CPU utilization", LABELS),
+            "DatabaseConnections"          : Gauge("rds_exporter_maria_database_connections_count", "The number of database connections in use", LABELS),
+            "SwapUsage"                    : Gauge("rds_exporter_maria_swap_usage_bytes", "The amount of swap space used on the DB instance", LABELS),
+            "FreeableMemory"               : Gauge("rds_exporter_maria_freeable_memory_bytes", "Available RAM", LABELS),
+            "BinLogDiskUsage"              : Gauge("rds_exporter_maria_binlog_disk_usage_bytes", "The amount of disk space occupied by binary logs on the master", LABELS)
+        },
+
+        "aurora": {
+            "Queries"                       : Gauge("rds_exporter_aurora_queries_per_second", "The average number of queries executed per second", LABELS),
+            "Deadlocks"                     : Gauge("rds_exporter_aurora_deadlock_per_second", "The average number of deadlocks in the database per second", LABELS),
+            "DeleteLatency"                 : Gauge("rds_exporter_aurora_delete_latency_milliseconds", "The amount of latency for delete queries", LABELS),
+            "DMLThroughput"                 : Gauge("rds_exporter_aurora_dml_throughput_per_second", "The average number of inserts, updates, and deletes per second", LABELS),
+            "CommitLatency"                 : Gauge("rds_exporter_aurora_commit_latency_milliseconds", "The amount of latency for commit queries", LABELS),
+            "CommitThroughput"              : Gauge("rds_exporter_aurora_commit_throughput_per_second", "The average number of commit queries per second", LABELS),
+            "BlockedTransactions"           : Gauge("rds_exporter_aurora_blocked_transaction_per_second", "The average number of transactions in the database that are blocked per second", LABELS),
+            "SelectThroughput"              : Gauge("rds_exporter_aurora_select_throughput_per_second", "The average number of select queries per second", LABELS),
+            "SelectLatency"                 : Gauge("rds_exporter_aurora_select_latency_milliseconds", "The amount of latency select requests", LABELS),
+            "DDLLatency"                    : Gauge("rds_exporter_aurora_ddl_latency_milliseconds", "The amount of latency for data definition language (DDL) requests", LABELS),
+            "NetworkTransmitThroughput"     : Gauge("rds_exporter_aurora_network_transmit_throughput_bytes_per_second", "Network transmission throughput", LABELS),
+            "ActiveTransactions"            : Gauge("rds_exporter_aurora_active_transactions_per_second", "The average number of current transactions executing on an Aurora database instance per second", LABELS),
+            "EngineUptime"                  : Gauge("rds_exporter_aurora_engine_uptime_seconds", "The amount of time that the instance has been running", LABELS),
+            "CPUUtilization"                : Gauge("rds_exporter_aurora_cpu_utilization_percent", "The percentage of CPU utilization", LABELS),
+            "DMLLatency"                    : Gauge("rds_exporter_aurora_dml_latency_milliseconds", "The amount of latency for data manipulation language (DDL) requests", LABELS),
+            "DeleteThroughput"              : Gauge("rds_exporter_aurora_delete_throughput_per_second", "The average number of delete queries per second", LABELS),
+            "DatabaseConnections"           : Gauge("rds_exporter_aurora_database_connections_count", "The number of database connections in use", LABELS),
+            "FreeableMemory"                : Gauge("rds_exporter_aurora_freeable_memory_bytes", "Available RAM", LABELS),
+            "DDLThroughput"                 : Gauge("rds_exporter_aurora_ddl_throughput_per_second", "The average number of inserts, updates, and deletes per second", LABELS),
+            "InsertLatency"                 : Gauge("rds_exporter_aurora_insert_latency_milliseconds", "The amount of latency insert requests", LABELS),
+            "LoginFailures"                 : Gauge("rds_exporter_aurora_login_failures_per_second", "The average number of failed login attempts per second", LABELS),
+            "FreeLocalStorage"              : Gauge("rds_exporter_aurora_free_local_storage_bytes", "The amount of storage available for temporary tables and logs", LABELS),
+            "CPUCreditBalance"              : Gauge("rds_exporter_aurora_cpu_credit_count", "CPU credit count", LABELS),
+            "NetworkReceiveThroughput"      : Gauge("rds_exporter_aurora_network_receive_throughput_bytes_per_second", "Network receive throughput", LABELS),
+            "UpdateLatency"                 : Gauge("rds_exporter_aurora_update_latency_milliseconds", "The amount of latency update requests", LABELS),
+            "UpdateThroughput"              : Gauge("rds_exporter_aurora_update_throughput_per_second", "The average number of update queries per second", LABELS),
+            "AuroraReplicaLagMinimum"       : Gauge("rds_exporter_aurora_replica_lag_minimum_milliseconds", "The minimum amount of lag between the primary instance and each Aurora DB instance in the DB cluster", LABELS),
+            "AuroraReplicaLagMaximum"       : Gauge("rds_exporter_aurora_replica_lag_maximum_milliseconds", "The maximum amount of lag between the primary instance and each Aurora DB instance in the DB cluster", LABELS),
+            "AuroraReplicaLag"              : Gauge("rds_exporter_aurora_replica_lag_milliseconds", "The amount of lag when replicating updates from the primary instance", LABELS),
+            "AuroraBinlogReplicaLag"        : Gauge("rds_exporter_aurora_bin_log_replica_lag_seconds", "The amount of time a replica DB cluster running on Aurora with MySQL compatibility lags behind the source DB cluster", LABELS)
+        }
     }
-}
+
+    def __init__(self, period, db_name, db_engine, availability_zone, region="us-east-1", access_key=None, secret_access_key=None):
+        """
+
+        :param period: Time period of metrics in seconds
+        :param db_name: Name of DB instance
+        :param db_engine: Name of DB engine
+        :param availability_zone: Name of the AZ, For example, us-east-1d
+        :param region: Region of the DB instance
+        :param access_key:
+        :param secret_access_key:
+        """
+        self.db_name            = db_name
+        self.db_engine          = db_engine.lower()
+        self.availability_zone  = availability_zone
+        self.region             = region
+        self.period             = period
+
+        if (access_key is None and secret_access_key is None) or (len(access_key) == 0 and len(secret_access_key) == 0):
+            """If access keys are empty or if their length is 0, try loading credentials from instance roles or .aws directory
+            """
+            self.cloudwatch_client = boto3.client("cloudwatch",
+                                                  region_name=region)
+        else:
+            self.cloudwatch_client = boto3.client("cloudwatch",
+                                                  region_name=region,
+                                                  aws_access_key_id=access_key,
+                                                  aws_secret_access_key=secret_access_key)
+
+    def get_metrics_from_cloud_watch(self):
+        """Performs an API call to boto3 to get metrics for cloudwatch
+
+        :return: None or Dict with metrics
+        """
+        metric_data_queries = []
+
+        if self.db_engine == "maria" or self.db_engine == "aurora":
+            db_metrics_dict = self.METRICS.get(self.db_engine)
+
+            for metric in db_metrics_dict.keys():
+                metric_data_query_dict = {
+                    "Id": metric.lower(),
+                    "MetricStat": {
+                        "Metric": {
+                            "Namespace": self.NAMESPACE,
+                            "MetricName": metric,
+                            "Dimensions": [
+                                {
+                                    "Name": "DBInstanceIdentifier",
+                                    "Value": self.db_name
+                                }
+                            ]
+                        },
+
+                        "Period": self.period,
+                        "Stat": "Maximum"
+                    },
+                    "Label": metric,
+                    "ReturnData": True
+                }
+
+                metric_data_queries.append(metric_data_query_dict)
+
+            response = self.cloudwatch_client.get_metric_data(
+                MetricDataQueries=metric_data_queries,
+                StartTime=datetime.utcnow() - timedelta(minutes=int(self.period/60)),
+                EndTime=datetime.utcnow(),
+                ScanBy='TimestampDescending'
+            )
+            return response
+
+        else:
+            Logger.publish_log_error("DB engine not specified or not supported, Supported engines are aurora and maria")
+            return None
--- rds_exporter.py
+++ rds_exporter.py
@@ -1,27 +1,50 @@
 import time
-import random
 
 from prometheus_client import start_http_server
-from prometheus_client.core import Gauge, REGISTRY
 
 from Util.ConfigParser import ConfigParser
+from Metrics import Metrics
 from Logger.Logger import Logger
 
 
-a = Gauge("rds_metric", "hello", ["func_name"])
-
-def func1():
-    return random.random()
-
-
-def func2():
-    return random.random()
-
 config = ConfigParser("config.yaml").parse_config_file()
-Logger.publish_log_info("Started RDS Metric Exporter")
-start_http_server(config.get("PrometheusPort"))
+Logger.publish_log_info("Started RDS Metric Exporter on port: {}".format(config.get("PrometheusPort")))
+
+period = config.get("Period")
+port   = config.get("PrometheusPort")
+
+start_http_server(port)
 
 while True:
-    a.labels(func_name="func1").set(func1())
+    for db_instance_data in config.get("DBInstances"):
+        db_instance_identifier = db_instance_data.get("DBInstanceIdentifier")
+        region                 = "us-east-1" if db_instance_data.get("Region") is None or len(db_instance_data.get("Region")) == 0 else db_instance_data.get("Region")
+        access_key             = db_instance_data.get("AWSAccessKey")
+        secret_access_key      = db_instance_data.get("AWSSecretKey")
+        db_engine              = db_instance_data.get("DBEngine")
+        availability_zone      = db_instance_data.get("AvailabilityZone")
 
-    a.labels(func_name="func2").set(func2())
+        db_metric_object = Metrics(period=period,
+                                   db_name=db_instance_identifier,
+                                   db_engine=db_engine,
+                                   availability_zone=availability_zone,
+                                   region=region,
+                                   access_key=access_key,
+                                   secret_access_key=secret_access_key)
+
+        db_metrics = db_metric_object.get_metrics_from_cloud_watch()
+
+        METRICS = db_metric_object.METRICS.get(db_engine)
+        LABELS  = db_metric_object.LABELS
+
+        for metric_result in db_metrics.get("MetricDataResults"):
+            label = metric_result.get("Label")
+
+            if len(metric_result.get("Values")) > 0:
+                gauge = METRICS.get(label)
+
+                gauge.labels(db_name=db_instance_identifier,
+                             db_engine=db_engine,
+                             az_name=availability_zone).set(metric_result.get("Values")[0])
+
+    time.sleep(60)
